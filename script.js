7de08756edb04125b8a20f2ef54534ed90c692c5
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-30 14:14:33 +0600

    lesson 10 added

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 // login Area
-let loginArea = document.getElementById('login-area');
+const loginArea = document.getElementById('login-area');
 // transation Area
-let transactionArea = document.getElementById('transaction-area');
+const transactionArea = document.getElementById('transaction-area');
 
-//  login Button
+
+
+//  login Button Event Handler
 let loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function (event) {
     event.preventDefault();
     loginArea.style.display = "none";
     transactionArea.style.display = "block";
     console.log("None!");
+});
+
+// deposit Ammount
+const depositAmmount = document.getElementById('deposit-ammount').value;
+
+// withdraw Ammount
+const withdrawAmmount = document.getElementById('withdraw-ammount').value;
+
+//  Deposite Button Event Handler
+let depositBtn = document.getElementById('addDeposit');
+depositBtn.addEventListener('click', function (event) {
+console.log(depositAmmount);
 });
