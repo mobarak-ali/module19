d9b139082ac88f17ff24e2679dc2b0b81f0a16eb
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-30 17:38:32 +0600

    working on progress lesson12

--- script12.js
+++ script12.js
@@ -1,6 +1,3 @@
-
-
-
 //  login Button Event Handler
 let loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function (event) {
@@ -14,20 +11,17 @@ loginBtn.addEventListener('click', function (event) {
 });
 
 
-// withdraw Amount
-const withdrawAmount = document.getElementById('withdraw-amount').value;
-
 //  Deposite Button Event Handler
 let depositBtn = document.getElementById('addDeposit');
 depositBtn.addEventListener('click', function (event) {
     // deposit Amount
     const depositAmount = document.getElementById('deposit-amount').value;
     let depositNumber = parseFloat(depositAmount);
-    if(depositNumber.toString() != "NaN" ){
-        if(depositNumber < 0){
+    if (depositNumber.toString() != "NaN") {
+        if (depositNumber < 0) {
             depositNumber = 0;
         }
-        
+
         // Update Deposite
         updateSpanText('current-deposit', depositNumber)
 
@@ -39,10 +33,47 @@ depositBtn.addEventListener('click', function (event) {
 });
 
 
+// withdraw Amount
+// const withdrawAmount = document.getElementById('withdraw-amount').value;
+//  Withdraw Button Event Handler
+const withdrawBtn = document.getElementById('addWithdraw');
+withdrawBtn.addEventListener('click', function (event) {
+    // deposit Amount
+    const withdrawAmount = document.getElementById('withdraw-amount').value;
+    let withdrawNumber = parseFloat(withdrawAmount);
+    if (withdrawNumber.toString() != "NaN") {
+        if (withdrawNumber < 0) {
+            withdrawNumber = 0;
+        }
+
+        // Update Deposite
+        updateSpanText('current-withdraw', withdrawNumber);
+
+        // Update Balance
+        // updateSpanText('current-withdraw', withdrawNumber)
+    }
+
+    document.getElementById('withdraw-amount').value = "";
+});
+
+
 // update Balance
-function updateSpanText(id, depositNumber){
+function updateSpanText(id, depositNumber) {
     const current = document.getElementById(id).innerText;
-    const currentAmount  = parseFloat(current);
+    const currentAmount = parseFloat(current);
     const totalAmount = currentAmount + depositNumber;
     document.getElementById(id).innerText = totalAmount;
+}
+
+//button event handler
+
+function getInputNumber(id) {
+    const inputAmount = document.getElementById(id).value;
+    let inputNumber = parseFloat(inputAmount);
+    return inputNumber;
+    if (inputNumber.toString() != "NaN") {
+        if (inputNumber < 0) {
+            inputNumber = 0;
+        }
+    }
 }
