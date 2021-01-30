8a32566c5617e05bca83848132ab718e5ff2a4c5
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-30 13:20:29 +0600

    lesson 07 completed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // login Area
 let loginArea = document.getElementById('login-area');
+// transation Area
+let transactionArea = document.getElementById('transaction-area');
 
 //  login Button
 let loginBtn = document.getElementById('login');
 loginBtn.addEventListener('click', function (event) {
     event.preventDefault();
     loginArea.style.display = "none";
+    transactionArea.style.display = "block";
     console.log("None!");
 });
