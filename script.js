09688f50ace281be88f370ffaf385a60d5c3136e
Mobarak Ali <mobarak.ali@outlook.com>
2021-01-30 12:28:30 +0600

    script tag added

---
+++ script.js
@@ -0,0 +1,9 @@
+// login Area
+let loginArea = document.getElementById('login-area');
+
+//  login Button
+let loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function () {
+    loginArea.style.display = "none";
+    console.log("None!")
+});
